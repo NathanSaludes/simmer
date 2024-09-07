971284d6eb1a4707e82fbaa5fb1bb4a1950cb621
NathanSaludes <saludesnathaniel@gmail.com>
2024-09-07 13:56:41 +0800

    create initial portfolio page

---
+++ src/app/portfolio/page.tsx
@@ -0,0 +1,9 @@
+import Header from "@/components/Header";
+
+export default function Portfolio() {
+  return (
+    <div className="container min-h-dvh outline outline-2 outline-black">
+      <Header />
+    </div>
+  );
+}
